9a7503a8bb9a541db202b6e2dca76963f857cda5
EwenDC <39972488+EwenDC@users.noreply.github.com>
2022-03-06 12:38:05 +1000

    Simplified the skipping step logic so it's generic for all steps

--- src/features/setupSlice.ts
+++ src/features/setupSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import undoable, { GroupByFunction } from "redux-undo";
 import { AppThunk, RootState } from "../components/store";
-import { selectEnabledDecks } from "./deckSlice";
+import { selectDeckArray, selectEnabledDecks } from "./deckSlice";
 import { selectEnabledMaps } from "./mapSlice";
 import { getRandom } from "./reduxUtils";
 
@@ -23,6 +23,11 @@ export enum SetupStep {
   chooseHand,
 }
 
+export interface SkipStepInput {
+  step: SetupStep;
+  skip: boolean;
+}
+
 export interface FactionPoolEntry {
   factionCode: string;
   lockedByFaction?: string;
@@ -30,25 +35,18 @@ export interface FactionPoolEntry {
 
 export interface SetupState {
   currentStep: SetupStep;
+  skippedSteps: Map<SetupStep, boolean>;
   // chooseMap
   map: string | null;
   usePrintedSuits: boolean;
   useMapLandmark: boolean;
   // chooseDeck
-  chooseDeckSkipped: boolean;
   deck: string | null;
-  // setUpBots
-  doBotSetup: boolean;
   // seatPlayers
   playerCount: number;
   fixedFirstPlayer: boolean;
   // chooseLandmarks
-  chooseLandmarksSkipped: boolean;
   landmarkCount: number;
-  // chooseHirelings
-  chooseHirelingsSkipped: boolean;
-  // setUpHirelings
-  doHirelingSetup: boolean;
   // chooseFaction
   factionPool: FactionPoolEntry[];
   // setUpFaction
@@ -57,18 +55,14 @@ export interface SetupState {
 
 const initialState: SetupState = {
   currentStep: SetupStep.chooseExpansions,
+  skippedSteps: new Map(),
   map: null,
   usePrintedSuits: false,
   useMapLandmark: false,
-  chooseDeckSkipped: false,
   deck: null,
-  doBotSetup: false,
   playerCount: 4,
   fixedFirstPlayer: false,
-  chooseLandmarksSkipped: false,
   landmarkCount: 0,
-  chooseHirelingsSkipped: false,
-  doHirelingSetup: false,
   factionPool: [],
   faction: null,
 };
@@ -83,7 +77,14 @@ export const setupSlice = createSlice({
       state.currentStep = action.payload;
     },
     incrementStep: (state) => {
-      state.currentStep++;
+      let skipStep = false;
+      do {
+        state.currentStep++;
+        skipStep = state.skippedSteps.get(state.currentStep) ?? false;
+      } while (skipStep);
+    },
+    skipStep: (state, action: PayloadAction<SkipStepInput>) => {
+      state.skippedSteps.set(action.payload.step, action.payload.skip);
     },
     usePrintedSuits: (state, action: PayloadAction<boolean>) => {
       state.usePrintedSuits = action.payload;
@@ -94,25 +95,42 @@ export const setupSlice = createSlice({
     setMap: (state, action: PayloadAction<string>) => {
       state.map = action.payload;
     },
+    setDeck: (state, action: PayloadAction<string>) => {
+      state.deck = action.payload;
+    },
   },
 });
 
 export const {
   setStep,
   incrementStep,
+  skipStep,
   usePrintedSuits,
   useMapLandmark,
   setMap,
+  setDeck,
 } = setupSlice.actions;
 
 export const nextStep = (): AppThunk => (dispatch, getState) => {
   // Retrieve our setup state
   const setupParameters = selectSetupParameters(getState());
+  let doIncrementStep = true;
 
-  // Handle any special logic that fires at the end of each step, including logic for going to next step
+  // Handle any special logic that fires at the end of a step
   switch (setupParameters.currentStep) {
     case SetupStep.chooseExpansions:
-      dispatch(incrementStep());
+      // After locking in the chosen expansions, we need to calculate which steps can be skipped
+
+      // Do we need to choose a deck?
+      const decks = selectDeckArray(getState());
+      if (decks.length === 1) {
+        // Auto select the only deck
+        dispatch(setDeck(decks[0].code));
+        dispatch(skipStep({ step: SetupStep.chooseDeck, skip: true }));
+      } else {
+        // Make sure we do the choose deck step
+        dispatch(skipStep({ step: SetupStep.chooseDeck, skip: false }));
+      }
       break;
 
     case SetupStep.chooseMap:
@@ -124,11 +142,10 @@ export const nextStep = (): AppThunk => (dispatch, getState) => {
         // Chose a random map and move on to next step
         dispatch(setMap(getRandom(mapPool).code));
         dispatch(incrementStep());
+      } else {
+        // Invalid state, do not proceed
+        doIncrementStep = false;
       }
-      break;
-
-    case SetupStep.setupMap:
-      // Check if we need to skip the chose deck step
       break;
 
     case SetupStep.chooseDeck:
@@ -140,10 +157,10 @@ export const nextStep = (): AppThunk => (dispatch, getState) => {
         // Chose a random map and move on to next step
         dispatch(setMap(getRandom(deckPool).code));
         dispatch(incrementStep());
+      } else {
+        // Invalid state, do not proceed
+        doIncrementStep = false;
       }
-      break;
-
-    case SetupStep.setUpBots:
       break;
 
     case SetupStep.seatPlayers:
@@ -181,6 +198,12 @@ export const nextStep = (): AppThunk => (dispatch, getState) => {
         `Invalid current step ${setupParameters.currentStep}! Resetting current step to ${SetupStep.chooseExpansions}...`
       );
       dispatch(setStep(SetupStep.chooseExpansions));
+      doIncrementStep = false;
+  }
+
+  // Increment the step (unless we're in an invalid state)
+  if (doIncrementStep) {
+    dispatch(incrementStep());
   }
 };
 
