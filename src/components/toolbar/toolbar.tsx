15e5c0e4fc992b37587016706c5d6e1471cbb365
EwenDC <39972488+EwenDC@users.noreply.github.com>
2022-05-09 18:07:49 +1000

    Allowed focusing disabled toolbar buttons
    This massively simplifies the logic contained within toolbar, making this both an accessibility and maintainability win

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -7,37 +7,7 @@ import styles from "./toolbar.module.css";
 import { ReactComponent as UndoIcon } from "../../images/undo.svg";
 import { ReactComponent as RedoIcon } from "../../images/redo.svg";
 import { ReactComponent as NextIcon } from "../../images/next.svg";
-import { useEffect, useMemo, useRef, useState } from "react";
-
-interface ToolbarButton {
-  ref: React.RefObject<HTMLButtonElement>;
-  disabled: boolean;
-}
-
-const getFocusNext = (
-  toolbarButtons: ToolbarButton[],
-  currentIndex: number,
-  stepAmount: number,
-  originalIndex?: number
-): number => {
-  const maxIndex = toolbarButtons.length - 1;
-  let newIndex = currentIndex + stepAmount;
-  if (newIndex < 0) {
-    newIndex = maxIndex;
-  } else if (newIndex > maxIndex) {
-    newIndex = 0;
-  }
-
-  if (newIndex === originalIndex) return originalIndex;
-  if (toolbarButtons[newIndex].disabled)
-    return getFocusNext(
-      toolbarButtons,
-      newIndex,
-      stepAmount,
-      originalIndex ?? currentIndex
-    );
-  return newIndex;
-};
+import { useRef, useState } from "react";
 
 export const Toolbar: React.FC = () => {
   const { t } = useTranslation();
@@ -45,117 +15,108 @@ export const Toolbar: React.FC = () => {
   const { pastSteps, futureSteps, currentStep } =
     useAppSelector(selectFlowState);
   const [focusedIndex, setFocusedIndex] = useState(0);
-  const undoButton = useRef<HTMLButtonElement>(null);
-  const redoButton = useRef<HTMLButtonElement>(null);
-  const nextButton = useRef<HTMLButtonElement>(null);
-  const toolbarButtons: ToolbarButton[] = useMemo(
-    () => [
-      {
-        ref: undoButton,
-        disabled: pastSteps.length === 0,
-      },
-      {
-        ref: redoButton,
-        disabled: futureSteps.length === 0,
-      },
-      {
-        ref: nextButton,
-        disabled: currentStep >= SetupStep.setupEnd,
-      },
-    ],
-    [pastSteps.length, futureSteps.length, currentStep]
-  );
 
-  // Shift focus if it's stuck on a newly disabled button
-  useEffect(() => {
-    let newIndex;
-    if (
-      toolbarButtons[focusedIndex].ref.current === document.activeElement &&
-      toolbarButtons[focusedIndex].disabled
-    ) {
-      newIndex = getFocusNext(toolbarButtons, focusedIndex, +1);
-      setFocusedIndex(newIndex);
-      toolbarButtons[newIndex].ref.current?.focus();
-    }
-  }, [toolbarButtons, focusedIndex]);
-
-  const currentIndex = toolbarButtons[focusedIndex].disabled
-    ? getFocusNext(toolbarButtons, focusedIndex, +1)
-    : focusedIndex;
+  const buttonRefs = [
+    useRef<HTMLButtonElement>(null),
+    useRef<HTMLButtonElement>(null),
+    useRef<HTMLButtonElement>(null),
+  ];
 
   const onKeyDownHandler = (
     event: React.KeyboardEvent<HTMLButtonElement>,
     focusedIndex: number
   ) => {
-    const maxIndex = toolbarButtons.length - 1;
+    const maxIndex = buttonRefs.length - 1;
     let newIndex: number | undefined;
 
-    if (event.key === "ArrowRight") {
-      newIndex = getFocusNext(toolbarButtons, focusedIndex, +1);
-    } else if (event.key === "ArrowLeft") {
-      newIndex = getFocusNext(toolbarButtons, focusedIndex, -1);
-    } else if (event.key === "Home" && !toolbarButtons[0].disabled) {
+    if (event.key === "ArrowRight" || event.key === "ArrowDown") {
+      newIndex = focusedIndex + 1 > maxIndex ? 0 : focusedIndex + 1;
+    } else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
+      newIndex = focusedIndex - 1 < 0 ? maxIndex : focusedIndex - 1;
+    } else if (event.key === "Home") {
       newIndex = 0;
-    } else if (event.key === "End" && !toolbarButtons[maxIndex].disabled) {
+    } else if (event.key === "End") {
       newIndex = maxIndex;
     }
 
     if (newIndex != null) {
       event.preventDefault();
       setFocusedIndex(newIndex);
-      toolbarButtons[newIndex].ref.current?.focus();
+      buttonRefs[newIndex].current?.focus();
     }
   };
 
+  const undoDisabled = pastSteps.length === 0;
+  const redoDisabled = futureSteps.length === 0;
+  const nextDisabled = currentStep >= SetupStep.setupEnd;
+
   return (
     <footer className={styles.container}>
-      <nav className={styles.toolbar} role="toolbar">
+      <div className={styles.toolbar} role="toolbar">
         <button
-          className={classNames(styles.button, styles.left)}
-          ref={undoButton}
-          disabled={toolbarButtons[0].disabled}
-          onClick={() => {
-            dispatch(undoStep());
-            setFocusedIndex(0);
-          }}
+          className={classNames(styles.button, styles.left, {
+            [styles.disabled]: undoDisabled,
+          })}
+          ref={buttonRefs[0]}
+          onClick={
+            undoDisabled
+              ? undefined
+              : () => {
+                  dispatch(undoStep());
+                  setFocusedIndex(0);
+                }
+          }
           title={t("label.undo")}
+          aria-disabled={undoDisabled}
           // We have to override the tabbing logic to meet the standard of role "toolbar"
-          tabIndex={currentIndex === 0 ? 0 : -1}
+          tabIndex={focusedIndex === 0 ? 0 : -1}
           onKeyDown={(e) => onKeyDownHandler(e, 0)}
         >
           <UndoIcon className={styles.image} />
         </button>
         <button
-          className={classNames(styles.button, styles.left)}
-          ref={redoButton}
-          disabled={toolbarButtons[1].disabled}
-          onClick={() => {
-            dispatch(redoStep());
-            setFocusedIndex(1);
-          }}
+          className={classNames(styles.button, styles.left, {
+            [styles.disabled]: redoDisabled,
+          })}
+          ref={buttonRefs[1]}
+          onClick={
+            redoDisabled
+              ? undefined
+              : () => {
+                  dispatch(redoStep());
+                  setFocusedIndex(1);
+                }
+          }
           title={t("label.redo")}
+          aria-disabled={redoDisabled}
           // We have to override the tabbing logic to meet the standard of role "toolbar"
-          tabIndex={currentIndex === 1 ? 0 : -1}
+          tabIndex={focusedIndex === 1 ? 0 : -1}
           onKeyDown={(e) => onKeyDownHandler(e, 1)}
         >
           <RedoIcon className={styles.image} />
         </button>
         <button
-          className={classNames(styles.button, styles.right)}
-          ref={nextButton}
-          disabled={toolbarButtons[2].disabled}
-          onClick={() => {
-            dispatch(nextStep());
-            setFocusedIndex(2);
-          }}
+          className={classNames(styles.button, styles.right, {
+            [styles.disabled]: nextDisabled,
+          })}
+          ref={buttonRefs[2]}
+          onClick={
+            nextDisabled
+              ? undefined
+              : () => {
+                  dispatch(nextStep());
+                  setFocusedIndex(2);
+                }
+          }
+          aria-disabled={nextDisabled}
           // We have to override the tabbing logic to meet the standard of role "toolbar"
-          tabIndex={currentIndex === 2 ? 0 : -1}
+          tabIndex={focusedIndex === 2 ? 0 : -1}
           onKeyDown={(e) => onKeyDownHandler(e, 2)}
         >
           {t("label.nextStep")}
           <NextIcon className={styles.image} />
         </button>
-      </nav>
+      </div>
     </footer>
   );
 };
