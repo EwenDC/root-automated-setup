b75b0ce4b5b7e94563735b8b04af4e8bb5349e7b
EwenDC <39972488+EwenDC@users.noreply.github.com>
2023-06-04 17:29:36 +1000

    Moved to using an auto-generated service worker
    Also stopped react component generating SVG's appearing in the output, and reinstated eslint into the project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,57 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
+import noEmit from "rollup-plugin-no-emit";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgrPlugin(),
+    // Allow importing SVG as react components
+    svgrPlugin({
+      svgrOptions: {
+        memo: true,
+      },
+    }),
+    // Exclude most SVG files from the output since we just use them to generate react components
+    // (mask-icon.svg is the exception as it's used for the safari mask image)
+    noEmit({
+      match: (file) => /.svg$/.test(file) && !/mask-icon/.test(file),
+    }),
     splitVendorChunkPlugin(),
     VitePWA({
       injectRegister: "inline",
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "service-worker.ts",
-      injectManifest: {
-        globPatterns: ["**/*.{js,css,png}"],
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,ico,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.origin === "https://fonts.googleapis.com",
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "google-fonts-stylesheets",
+            },
+          },
+          {
+            urlPattern: ({ url }) => url.origin === "https://fonts.gstatic.com",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts-webfonts",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30,
+              },
+            },
+          },
+        ],
       },
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
         name: "Root Automated Setup",
         short_name: "Root Auset",
-        description: "Automate the Advanced Setup process for Leder Game's popular board game Root",
+        description: "Automate the Advanced Setup process for Leder Games popular board game Root",
         theme_color: "#2C2E35",
         background_color: "#FFFFF0",
         icons: [
