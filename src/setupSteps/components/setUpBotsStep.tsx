c65d09d74ec7fea3e367877d7276828d74bee665
NolanDonahue <nolandonahue0@gmail.com>
2026-03-25 07:44:42 -0400

    Forgot to hit save on these and the changes did not stage

--- src/setupSteps/components/setUpBotsStep.tsx
+++ src/setupSteps/components/setUpBotsStep.tsx
@@ -1,17 +1,11 @@
 import type { SetupStepComponent } from '..'
 
 import Section from '../../components/section'
-import { useAppSelector } from '../../hooks'
-import { selectBotArray } from '../../store'
 
 const SetUpBotsStep: SetupStepComponent = ({ flowSlice }) => {
   const { selectedBots } = flowSlice
-  const allBots = useAppSelector(selectBotArray)
 
   const botCode = selectedBots.length > 0 ? selectedBots[selectedBots.length - 1] : null
-  const bot = allBots.find(b => b.code === botCode)
-
-  console.log(bot?.baseFactionCode)
 
   if (!botCode) return null
 
